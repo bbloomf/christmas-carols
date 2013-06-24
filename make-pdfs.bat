ff11268cd8424ad44e838ee4fbbbec976de65a77
Benjamin Bloomfield <bhb123+g@gmail.com>
2013-06-24 15:12:01 -0400

    Added extra artwork

---
+++ make-pdfs.bat
@@ -0,0 +1,11 @@
+gswin64c -o ccc.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [27 18 585 774] /PAGES pdfmark" -f !full.pdf pdfmarks.txt
+rem copy !full.pdf 9781484022801_content.pdf
+pdftk A=!full.pdf cat Aodd output odd.pdf
+pdftk A=!full.pdf cat Aeven output even.pdf
+gswin64c -o !odd.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [72 18 558 774] /PAGES pdfmark" -f odd.pdf
+gswin64c -o !even.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [54 18 540 774] /PAGES pdfmark" -f even.pdf
+
+pdftk A=!odd.pdf B=!even.pdf shuffle A B output !ccc-playbook.pdf
+
+gswin64c -o ccc-playbook.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [27 18 585 774] /PAGES pdfmark" -f !ccc-playbook.pdf pdfmarks.txt
+del even.pdf odd.pdf !even.pdf !odd.pdf !ccc-playbook.pdf
