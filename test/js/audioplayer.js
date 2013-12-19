0adc7d8a46617eccdc73d4db670febb611d8d42e
Benjamin Bloomfield <bhb123+g@gmail.com>
2013-12-19 09:56:46 -0500

    Testing

--- test/js/audioplayer.js
+++ test/js/audioplayer.js
@@ -2,7 +2,7 @@ $(function(){
   var playMIDI = function(url) {
     player = MIDI.Player;
     player.timeWarp = 1; // speed the song is played back
-    player.loadFile(data, player.start);
+    player.loadFile(url, player.start);
   };
   window.playMIDI = playMIDI;
 });
