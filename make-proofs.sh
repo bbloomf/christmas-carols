515dccc4f5cae53a64c8479f194428a8a4f51199
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2015-11-20 12:03:20 -0500

    Updated pdfmarks Updated make-proofs-sh to work on Unix

--- make-proofs.sh
+++ make-proofs.sh
@@ -7,17 +7,17 @@ if git pull
 then
   cp \!full.pdf 9781475217551_content.pdf
   echo 'Adding bookmarks to 8.5x11 version'
-  gswin64c -o gh-pages/pdfs/ccc.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -f \!full.pdf pdfmarks.txt
+  gs -o gh-pages/pdfs/ccc.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -f \!full.pdf pdfmarks.txt
   echo 'Extracting odd pages'
   pdftk A=\!full.pdf cat Aodd output odd.pdf
   echo 'Extracting even pages'
   pdftk A=\!full.pdf cat Aeven output even.pdf
-  gswin64c -o \!odd.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [72 18 558 774] /PAGES pdfmark" -f odd.pdf
-  gswin64c -o \!even.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [54 18 540 774] /PAGES pdfmark" -f even.pdf
+  gs -o \!odd.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [72 18 558 774] /PAGES pdfmark" -f odd.pdf
+  gs -o \!even.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [54 18 540 774] /PAGES pdfmark" -f even.pdf
 
   pdftk A=\!odd.pdf B=\!even.pdf shuffle A B output \!ccc-playbook.pdf
 
-  if gswin64c -o gh-pages/pdfs/ccc-playbook.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [27 18 585 774] /PAGES pdfmark" -f \!ccc-playbook.pdf pdfmarks.txt
+  if gs -o gh-pages/pdfs/ccc-playbook.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [27 18 585 774] /PAGES pdfmark" -f \!ccc-playbook.pdf pdfmarks.txt
   then
     pushd gh-pages
     if git add pdfs/ccc-playbook.pdf pdfs/ccc.pdf
@@ -38,7 +38,7 @@ then
     fi
     popd
   else
-    echo 'Error from gswin64c --^'
+    echo 'Error from gs --^'
   fi
   rm even.pdf odd.pdf \!even.pdf \!odd.pdf \!ccc-playbook.pdf
 else
